13199cea475158a195f787f567c6cf00686f16fd
Caroline Berg <carolineeberg@gmail.com>
2019-01-25 17:37:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,9 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(name, breed, id=nil)
-
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end
 
 
